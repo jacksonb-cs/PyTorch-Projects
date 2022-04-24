2e18f3d5a8e232817c8f2011d97454a6cb24b462
Jackson Bullard <jsbullar@uark.edu>
2022-04-24 12:27:58 -0500

    Break.

--- tutorial/DataTutorial.py
+++ tutorial/DataTutorial.py
@@ -18,4 +18,24 @@ test_data = datasets.FashionMNIST(
 	transform=ToTensor()
 )
 
+labels_map = {
+	0: "T-Shirt",
+	1: "Trouser",
+	2: "Pullover",
+	3: "Dress",
+	4: "Coat",
+	5: "Sandal",
+	6: "Shirt",
+	7: "Sneaker",
+	8: "Bag",
+	9: "Ankle Boot",
+}
+
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+
+for i in range(1, cols * rows + 1):
+	sample_idx = torch.randint(len(training_data), size=(1,)).item()
+	img, label = training_data[sample_idx]
+
 print('\n========== DONE ==========\n')

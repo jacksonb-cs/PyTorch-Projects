499f42f9293c1a7a0a7dc5f2ad4fb05d80d3d7a4
Jackson Bullard <jsbullar@uark.edu>
2022-04-28 20:55:29 -0500

    Data tutorial complete!

--- tutorial/DataTutorial.py
+++ tutorial/DataTutorial.py
@@ -1,8 +1,42 @@
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+
 import torch
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
 from torchvision import datasets
 from torchvision.transforms import ToTensor
-import matplotlib.pyplot as plt
+from torchvision.io import read_image
+
+class CustomImgDataset(Dataset):
+
+	def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+
+		self.img_labels = pd.read_csv(annotations_file)
+		self.img_dir = img_dir
+		self.transform = transform
+		self.target_transform = target_transform
+
+	def __len__(self):
+
+		return len(self.img_labels)
+
+	def __getitem__(self, idx):
+
+		img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+		image = read_image(img_path)
+		label = self.img_labels.iloc[idx, 1]
+
+		if self.transform:
+			image = self.transform(image)
+
+		if self.target_transform:
+			label = self.target_transform(label)
+
+		return image, label
+
 
 training_data = datasets.FashionMNIST(
 	root='data',
@@ -31,11 +65,36 @@ labels_map = {
 	9: "Ankle Boot",
 }
 
-figure = plt.figure(figsize=(8, 8))
-cols, rows = 3, 3
+batch_size = 64
+train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+test_dataLoader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
-for i in range(1, cols * rows + 1):
-	sample_idx = torch.randint(len(training_data), size=(1,)).item()
-	img, label = training_data[sample_idx]
+# figure = plt.figure(figsize=(8, 8))
+# cols, rows = 3, 3
 
-print('\n========== DONE ==========\n')
+# for i in range(1, cols * rows + 1):
+
+# 	sample_idx = torch.randint(len(training_data), size=(1,)).item()
+# 	img, label = training_data[sample_idx]
+# 	figure.add_subplot(rows, cols, i)
+
+# 	plt.title(labels_map[label])
+# 	plt.axis('off')
+# 	plt.imshow(img.squeeze(), cmap='gray')
+
+# plt.show()
+
+# Display image and label
+train_features, train_labels = next(iter(train_dataloader))
+
+print(f'Feature batch shape: {train_features.size()}')
+print(f'Labels batch shape: {train_labels.size()}')
+
+img = train_features[0].squeeze()
+label = train_labels[0]
+
+plt.imshow(img, cmap='gray')
+plt.show()
+print(f'Label: {labels_map[label.item()]}')
+
+print('\n========== DONE ==========')
